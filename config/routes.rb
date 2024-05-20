1aad7147931cac6ee37632b3d2bac506606d4937
sutantoc <165302919+sutantoc@users.noreply.github.com>
2024-05-20 20:32:10 +0000

    execute generated migrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
+  # Routes for the Post resource:
+
+  # CREATE
+  post("/insert_post", { :controller => "posts", :action => "create" })
+          
+  # READ
+  get("/posts", { :controller => "posts", :action => "index" })
+  
+  get("/posts/:path_id", { :controller => "posts", :action => "show" })
+  
+  # UPDATE
+  
+  post("/modify_post/:path_id", { :controller => "posts", :action => "update" })
+  
+  # DELETE
+  get("/delete_post/:path_id", { :controller => "posts", :action => "destroy" })
+
+  #------------------------------
+
   # Routes for the Board resource:
 
   # CREATE
