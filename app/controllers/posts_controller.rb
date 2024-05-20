63cbc3211c1e526a30bd2c6600a4d0c913f55fbe
sutantoc <165302919+sutantoc@users.noreply.github.com>
2024-05-20 21:28:07 +0000

    validations

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+
+
   def index
     matching_posts = Post.all
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -11,5 +11,10 @@
 #  board_id   :integer
 #
 class Post < ApplicationRecord
+  validates(:title, presence: true)
+  validates(:body, { :presence => true })
+  validates(:expires_on, { :presence => true })
+  validates(:board_id, { :presence => true })
+
   belongs_to(:board, class_name: "Board", foreign_key: "board_id")
 end
